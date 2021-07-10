5c65fc729de461b32747ebd422d9e697b98350be
michaelcmelton <michaelcmelton93@gmail.com>
2021-07-09 20:44:47 -0400

    fix: Correct Imports on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../styles/index.scss";
-import App from "../src/components/App";
+import "./styles/index.scss";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
